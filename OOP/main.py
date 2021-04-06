9efa319b7b0bdeb08ced39014c61d578bea50966
Melkior0 <81634078+Melkior0@users.noreply.github.com>
2021-04-06 17:47:26 +0200

    Added camion class

--- OOP/main.py
+++ OOP/main.py
@@ -4,3 +4,21 @@ class Vehicle:
         self.horse_power = horse_power
         self.weight = weight
 
+class Camion(Vehicle):
+    def __init__(self,nb_de_roues,nb_de_roues_motrices,size,horse_power,weight):
+        self.nb_de_roues=nb_de_roues
+        self.nb_de_roues_motrices=nb_de_roues_motrices
+        self.size = size
+        self.horse_power = horse_power
+        self.weight = weight
+
+    def puissance_par_roues_motrices(self):
+        pow=self.horse_power/self.nb_de_roues_motrices
+        print("La puissance par roue est de", pow,"!")
+    
+    def nb_de_roues_pas_motrices(self):
+        N=self.nb_de_roues-self.nb_de_roues_motrices
+        print("Ce camion a ",N," roues motrices!")
+
+
+
