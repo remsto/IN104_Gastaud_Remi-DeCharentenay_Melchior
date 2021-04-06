2d68916c7db55c87f9efecdbe3eaa7a7ddfceab0
Rémi Gastaud <remi.gastaud@ensta-paris.fr>
2021-04-06 17:51:50 +0200

    added submarine class

--- OOP/main.py
+++ OOP/main.py
@@ -4,3 +4,14 @@ class Vehicle:
         self.horse_power = horse_power
         self.weight = weight
 
+class Submarine(Vehicle):
+    def __init__(self, size, horse_power, weight, max_depth, nb_propeller):
+        self.size = size
+        self.horse_power = horse_power
+        self.weight = weight
+        self.max_depth = max_depth
+        self.nb_propeller = nb_propeller
+    def getPowerPerPropeller(self):
+        return self.horse_power/self.nb_propeller
+    def getSize(self):
+        return self.size
