5b415763d9eaa059cbac5c6b8a1338546cfc9b7a
Rémi Gastaud <remi.gastaud@ensta-paris.fr>
2021-04-10 19:25:30 +0200

    added method verification for subclasses

--- OOP/main.py
+++ OOP/main.py
@@ -4,6 +4,7 @@ class Vehicle:
         self.horse_power = horse_power
         self.weight = weight
 
+
 class Submarine(Vehicle):
     def __init__(self, size, horse_power, weight, max_depth, nb_propeller):
         self.size = size
@@ -11,26 +12,43 @@ class Submarine(Vehicle):
         self.weight = weight
         self.max_depth = max_depth
         self.nb_propeller = nb_propeller
+
     def getPowerPerPropeller(self):
         return self.horse_power/self.nb_propeller
+
     def getSize(self):
         return self.size
 
+
 class Camion(Vehicle):
-    def __init__(self,nb_de_roues,nb_de_roues_motrices,size,horse_power,weight):
-        self.nb_de_roues=nb_de_roues
-        self.nb_de_roues_motrices=nb_de_roues_motrices
+    def __init__(self, nb_de_roues, nb_de_roues_motrices, size, horse_power, weight):
+        self.nb_de_roues = nb_de_roues
+        self.nb_de_roues_motrices = nb_de_roues_motrices
         self.size = size
         self.horse_power = horse_power
         self.weight = weight
 
     def PuissanceParRouesMotrices(self):
-        pow=self.horse_power/self.nb_de_roues_motrices
-        print("La puissance par roue est de", pow,"!")
-    
+        pow = self.horse_power/self.nb_de_roues_motrices
+        return pow
+
     def NbDeRouesPasMotrices(self):
-        N=self.nb_de_roues-self.nb_de_roues_motrices
-        print("Ce camion a ",N," roues motrices!")
+        N = self.nb_de_roues-self.nb_de_roues_motrices
+        return N
 
 
+foudroyant = Submarine(20, 500, 600, 10000, 4)
 
+# Expected value : 125
+print(foudroyant.getPowerPerPropeller())
+
+# Expected value : 20
+print(foudroyant.getSize())
+
+camion_rouge = Camion(8,4,10,150,2000)
+
+# Expected value : 37.5
+print(camion_rouge.PuissanceParRouesMotrices())
+
+# Expected value : 4
+print(camion_rouge.NbDeRouesPasMotrices())
