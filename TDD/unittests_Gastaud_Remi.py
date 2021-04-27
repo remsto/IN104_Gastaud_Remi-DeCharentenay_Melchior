2a1f6d7aa85f23778adb97dc6a2960231c10109c
Rémi Gastaud <remi.gastaud@ensta-paris.fr>
2021-04-27 17:34:45 +0200

    added some tests

--- TDD/unittests_Gastaud_Remi.py
+++ TDD/unittests_Gastaud_Remi.py
@@ -1,1 +1,17 @@
-import .
+import main
+import unittest
+
+class Test(unittest.TestCase):
+    
+    def test_PuissanceParRoueMotrices(self):
+        camion_vert = main.Camion(8,4,10,150,2000)
+        self.assertEqual(camion_vert.PuissanceParRouesMotrices(),37.5);
+    def test_NbDeRouesPasMotrices(self):
+        camion_vert = main.Camion(8,4,10,150,2000)
+        self.assertEqual(camion_vert.NbDeRouesPasMotrices(),4)
+    def test_nb_de_roues_negatif(self):
+        camion_vert = main.Camion(8,4,10,150,2000)
+        self.assertRaises(main.BadArgumentsError,camion_vert.NbDeRouesPasMotrices(),[])
+
+if __name__ == '__main__':
+    unittest.main()
