0f3015ea94d89258c199939a7e04989b3fc1986e
Rémi Gastaud <remi.gastaud@ensta-paris.fr>
2021-05-04 13:24:16 +0200

    added gravite test

--- TDD/unittests_Gastaud_Remi.py
+++ TDD/unittests_Gastaud_Remi.py
@@ -5,10 +5,13 @@ class Test(unittest.TestCase):
     
     def test_PuissanceParRoueMotrices(self):
         camion_vert = main.Camion(8,4,10,150,2000)
-        self.assertEqual(camion_vert.PuissanceParRouesMotrices(),37.5);
+        self.assertEqual(camion_vert.PuissanceParRouesMotrices(),37.5)
     def test_NbDeRouesPasMotrices(self):
         camion_vert = main.Camion(8,4,10,150,2000)
         self.assertEqual(camion_vert.NbDeRouesPasMotrices(),4)
+    def test_ForceGravite(self):
+        camion_vert = main.Camion(8,4,10,150,2000)
+        self.assertEqual(camion_vert.ForceGravite(),19620)
     def test_nb_de_roues_negatif(self):
         camion_vert = main.Camion(8,4,10,150,2000)
         self.assertRaises(main.BadArgumentsError,camion_vert.NbDeRouesPasMotrices)
