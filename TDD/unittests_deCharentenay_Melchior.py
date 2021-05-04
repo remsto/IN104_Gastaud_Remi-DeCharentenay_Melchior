4d16412f026cf8ff61255fe098403ba6691bfdff
Melkior0 <81634078+Melkior0@users.noreply.github.com>
2021-05-04 13:41:20 +0200

    caps

--- TDD/unittests_deCharentenay_Melchior.py
+++ TDD/unittests_deCharentenay_Melchior.py
@@ -3,16 +3,16 @@ import main
 
 class Test(unittest.TestCase):
     def test_getPowerPerPropeller(self):
-        sous_marin = main.submarine(20, 500, 600, 10000, 4)
+        sous_marin = main.Submarine(20, 500, 600, 10000, 4)
         self.assertEqual(sous_marin.getPowerPerPropeller(),125)
     def test_getSize(self):
-        sous_marin = main.submarine(20, 500, 600, 10000, 4)
+        sous_marin = main.Submarine(20, 500, 600, 10000, 4)
         self.assertEqual(sous_marin.getSize(),20)
     def test_taille_negatif(self):
-        sous_marin = main.submarine(20, 500, 600, 10000, 4)
+        sous_marin = main.Submarine(20, 500, 600, 10000, 4)
         self.assertRaises(main.BadArgumentsError,sous_marin.getSize)
     def test_getPuissance(self):
-        sous_marin = main.submarine(20, 500, 600, 10000, 4)
+        sous_marin = main.Submarine(20, 500, 600, 10000, 4)
         self.assertEqual(sous_marin.GetPuissance(),20)
 
 
